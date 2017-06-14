8e15dd3d71905ba2b1221013ba33593dd063e029
Rohit <rohit_sl@yahoo.com>
2017-06-14 07:21:52 +0530

    TestNG and reporting changes

---
+++ src/main/java/com/rohit/practice/page/util/ExplicitWait.java
@@ -0,0 +1,8 @@
+package com.rohit.practice.page.util;
+
+/**
+ * Created by rohit on 08-May-17.
+ */
+public interface ExplicitWait {
+
+}
---
+++ src/main/java/com/rohit/practice/page/util/SearchScope.java
@@ -0,0 +1,17 @@
+package com.rohit.practice.page.util;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.SearchContext;
+
+import java.util.Optional;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+/**
+ * Created by rohit on 08-May-17.
+ */
+
+public interface SearchScope {
+
+}
