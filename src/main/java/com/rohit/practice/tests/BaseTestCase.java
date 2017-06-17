dca7d9a84517b8d170ecabd381e985419c2580d6
Rohit <rohit_sl@yahoo.com>
2017-06-17 17:28:22 +0530

    TestNG and reporting changes

---
+++ src/main/java/com/rohit/practice/tests/BaseTestCase.java
@@ -0,0 +1,13 @@
+package com.rohit.practice.tests;
+
+import com.rohit.practice.page.UIMap;
+import com.rohit.practice.page.WebPage;
+
+/**
+ * Created by rohit on 07-May-17.
+ */
+public abstract class BaseTestCase <T extends WebPage<UIMap>> {
+
+
+
+}
